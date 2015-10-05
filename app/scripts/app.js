a7f7fcbbd8462bccd983606c7d286a72cea06df1
Jeremy Ferry <jeremy.ferry@gmail.com>
2015-10-05 15:20:16 -0700

    refactored to use recommended style

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,11 @@
 'use strict';
     
-var app = angular.module('app', ['ngRoute']);
+angular
+    .module('app', ['ngRoute'])
 
-// configure our routes
-app.config(function ($routeProvider) {
-    $routeProvider
+    // configure our routes
+    .config(function ($routeProvider) {
+        $routeProvider
 
         .when('/', {
             templateUrl : 'views/home.html',
@@ -30,69 +31,74 @@ app.config(function ($routeProvider) {
             templateUrl : 'views/contact.html',
             controller  : 'contactCtrl'
         });
-});
+})
+    // add controllers
+    .controller('mainCtrl', MainCtrl)
+    .controller('aboutCtrl', AboutCtrl )
+    .controller('contactCtrl', ContactCtrl);
 
-app.controller('mainCtrl', function($scope) {
+
+function MainCtrl($scope) {
     $scope.message = 'Welcome to Job Search Log!';
         
-//    var request = indexedDB.open("JobSearchLog");
-//    
-//    request.onupgradeneeded = function() {
-//        // The database did not previously exist, so create object stores and indexes.
-//        var db = request.result;
-//        var store = db.createObjectStore("activities", {keyPath: "id", autoIncrement: true});
-//        var idIndex = store.createIndex("by_id", "id", {unique: true});
-//        var employerIndex = store.createIndex("by_employer", "employer");
-//
-//        // Populate with initial data.
-//        store.put({
-//            id: 1,
-//            contactDate: "08/14/2015",
-//            contactType: "employer",
-//            contactMethod: "online",
-//            activityType: "application",
-//            position: "Sr. Software Engineer",
-//            employer: "Tableau",
-//            contact: "joe@example.com",
-//            jobRefNumber: "12345"
-//        });
-//        store.put({
-//            id: 2,
-//            contactDate: "08/14/2015",
-//            contactType: "employer",
-//            contactMethod: "online",
-//            activityType: "application",
-//            position: "Software Engineer",
-//            employer: "Google",
-//            contact: "bob@example.com",
-//            jobRefNumber: "BR1245"
-//        });
-//        store.put({
-//            id: 3,
-//            contactDate: "08/14/2015",
-//            contactType: "employer",
-//            contactMethod: "online",
-//            activityType: "application",
-//            position: "Software Engineer, Java",
-//            employer: "Adobe",
-//            contact: "tom@example.com",
-//            jobRefNumber: "5432A"
-//        });
-//    };
-//
-//    request.onsuccess = function() {
-//        db = request.result;
-//    };
+    var request = indexedDB.open("JobSearchLog");
+    var db;
+    
+    request.onupgradeneeded = function() {
+        // The database did not previously exist, so create object stores and indexes.
+        db = request.result;
+        var store = db.createObjectStore("activities", {keyPath: "id", autoIncrement: true});
+        var idIndex = store.createIndex("by_id", "id", {unique: true});
+        var employerIndex = store.createIndex("by_employer", "employer");
+
+        // Populate with initial data.
+        store.put({
+            id: 1,
+            contactDate: "08/14/2015",
+            contactType: "employer",
+            contactMethod: "online",
+            activityType: "application",
+            position: "Sr. Software Engineer",
+            employer: "Tableau",
+            contact: "joe@example.com",
+            jobRefNumber: "12345"
+        });
+        store.put({
+            id: 2,
+            contactDate: "08/14/2015",
+            contactType: "employer",
+            contactMethod: "online",
+            activityType: "application",
+            position: "Software Engineer",
+            employer: "Google",
+            contact: "bob@example.com",
+            jobRefNumber: "BR1245"
+        });
+        store.put({
+            id: 3,
+            contactDate: "08/14/2015",
+            contactType: "employer",
+            contactMethod: "online",
+            activityType: "application",
+            position: "Software Engineer, Java",
+            employer: "Adobe",
+            contact: "tom@example.com",
+            jobRefNumber: "5432A"
+        });
+    };
+
+    request.onsuccess = function() {
+        db = request.result;
+    };
         
-});
+};
 
 
-
-
-app.controller('aboutCtrl', function($scope) {
+function AboutCtrl($scope) {
     $scope.message = 'This app is for logging your job search activities.';
-});
+};
 
-app.controller('contactCtrl', function($scope) {
+
+function ContactCtrl($scope) {
     $scope.message = 'Put contact info here eventually...';
-});
+};

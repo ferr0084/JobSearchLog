656f6b7e49eb5480d2ad976076da05736b7a301c
Jeremy Ferry <jeremy.ferry@gmail.com>
2015-10-06 10:08:32 -0700

    fixed form display issues, inserting a new activity now works

--- app/scripts/controllers/newActivityCtrl.js
+++ app/scripts/controllers/newActivityCtrl.js
@@ -5,17 +5,61 @@ angular
     .controller('newActivityCtrl', ['$scope', '$http', NewActivityCtrl]);
                                     
 function NewActivityCtrl($scope, $http) {    
-//    $scope.addActivity = function(){
-//    $scope.activities.push({
-//        id: 4,
-//        contactDate: $scope.contactDate,
-//        contactType: $scope.contactType,
-//        contactMethod: $scope.contactMethod,
-//        activityType: $scope.activityType,
-//        position: $scope.position,
-//        employer: $scope.employer,
-//        contact: $scope.contact,
-//        jobRefNumber: $scope.jobRefNumber
-//      });
-//    };
+
+    $scope.addActivity = function(){
+        
+        var req = indexedDB.open("JobSearchLog");
+    
+        req.onsuccess = function(e){
+            var db = e.target.result;
+            var tx = db.transaction("activities", "readwrite");
+            var store = tx.objectStore("activities");
+
+            store.put({
+                id: 5,
+                contactDate: $scope.contactDate,
+                activityType: $scope.activityType,
+                contactType: $scope.contactType,
+                contactMethod: $scope.contactMethod,
+                other: $scope.other,
+                employer: $scope.employer,
+                position: $scope.position,
+                phone: $scope.phone,
+                address: $scope.address,
+                city: $scope.city,
+                state: $scope.state,
+                contact: $scope.contact,
+                website: $scope.website,
+                email: $scope.email,
+                jobRefNumber: $scope.jobRefNumber,
+                fax: $scope.fax,
+                newspaperName: $scope.newspaperName,
+                publicationDate: $scope.publicationDate,
+                activityDescription: $scope.activityDescription
+            });
+    
+            tx.oncomplete = function() {
+                $scope.contactDate = '';
+                $scope.activityType = '';
+                $scope.contactType = '';
+                $scope.contactMethod = '';
+                $scope.other = '';
+                $scope.employer = '';
+                $scope.position = '';
+                $scope.phone = '';
+                $scope.address = '';
+                $scope.city = '';
+                $scope.state = '';
+                $scope.contact = '';
+                $scope.website = '';
+                $scope.email = '';
+                $scope.jobRefNumber = '';
+                $scope.fax = '';
+                $scope.newspaperName = '';
+                $scope.publicationDate = '';
+                $scope.activityDescription = '';
+            };
+            
+        };    
+    };
 };

372572f37732140e043f1d35abfb290733efbd49
Jeremy Ferry <jeremy.ferry@gmail.com>
2015-10-05 17:33:22 -0700

    the page is now populated with data from the indexeddb

--- app/scripts/controllers/activityListCtrl.js
+++ app/scripts/controllers/activityListCtrl.js
@@ -6,43 +6,25 @@ angular
                 
 function ActivityListCtrl($scope, $http){
     
-      $http.get('../../data/activities.json').success(function(data){
-         $scope.activities = data; 
-      }).error(function(data){
-            $http.get('/app/data/activities.json').success(function(data){
-                $scope.activities = data; 
-            });
-      });
-        
-//    var db = indexedDB.open("JobSearchLog").request.result;
-//    var tx = db.transaction("activities", "readonly");
-//    var store = tx.objectStore("activities");
-//    var index = store.index("by_id");
-//    $scope.activities = [];       
-//
-//    var request = index.openCursor();
-//    request.onsuccess = function($scope) {
-//        var cursor = request.result;
-//        if (cursor) {
-//            // Called for each matching record.
-//            $scope.activities.push(cursor.value);
-//    
-//            report(
-//                cursor.value.id, 
-//                cursor.value.contactDate, 
-//                cursor.value.contactType, 
-//                cursor.value.contactMethod, 
-//                cursor.value.activityType, 
-//                cursor.value.position, 
-//                cursor.value.employer, 
-//                cursor.value.contact, 
-//                cursor.value.jobRefNumber);
-//      
-//            cursor.continue();
-//        } else {
-//            // No more matching records.
-//            report(null);
-//        }
-//    }
+    $scope.activities = [];
+    var req = indexedDB.open("JobSearchLog");
+    
+    req.onsuccess = function(e){
+        var db = e.target.result;
+        var tx = db.transaction("activities", "readonly");
+        var store = tx.objectStore("activities");
+        var index = store.index("by_id");
+
+        var request = index.openCursor();
+
+        request.onsuccess = function(e) {
+            var cursor = e.target.result;
+            if (cursor) {
+                // Called for each matching record.
+                $scope.activities.push(cursor.value);
+                cursor.continue();
+            } 
+        }
+    };
         
 };

5f957e454d285c15e636db174dd051b8176527c8
Jeremy Ferry <jeremy.ferry@gmail.com>
2015-10-06 22:33:06 -0700

    added column for action buttons, implemented deleteActivity

--- app/scripts/controllers/activityListCtrl.js
+++ app/scripts/controllers/activityListCtrl.js
@@ -5,7 +5,7 @@ angular
     .controller('activityListCtrl', [ '$scope', '$http', ActivityListCtrl ]);
                 
 function ActivityListCtrl($scope, $http){
-    
+
     $scope.activities = [];
     var req = indexedDB.open("JobSearchLog");
     
@@ -14,9 +14,7 @@ function ActivityListCtrl($scope, $http){
         var tx = db.transaction("activities", "readonly");
         var store = tx.objectStore("activities");
         var index = store.index("by_id");
-
         var request = index.openCursor();
-
         request.onsuccess = function(e) {
             var cursor = e.target.result;
             if (cursor) {
@@ -26,5 +24,37 @@ function ActivityListCtrl($scope, $http){
             } 
         }
     };
-        
+
+    $scope.openActivity = function(id){
+        window.alert("clicked open " + id);
+    };
+
+    $scope.editActivity = function(id){
+        window.alert("clicked edit " + id);
+    };
+
+    $scope.deleteActivity = function(id){
+        var req = indexedDB.open("JobSearchLog");
+    
+        req.onsuccess = function(e){
+            var db = e.target.result;
+            var tx = db.transaction("activities", "readwrite");
+            var store = tx.objectStore("activities");
+            var index = store.index("by_id");
+
+            var request = index.get(id);
+            request.onsuccess = function() {
+                var matching = request.result;
+                if (matching !== undefined) {
+                    // A match was found.
+                    store.delete(id);
+                } else {
+                    // No match was found.
+                    alert('no record with id ' + id + ' found');
+                }
+            };
+        };
+
+    };
+    
 };

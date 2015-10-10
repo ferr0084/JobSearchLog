a10bf9aa9dbaca9bc898d465edd594ffe62873e3
Jeremy Ferry <jeremy.ferry@gmail.com>
2015-10-10 12:36:34 -0700

    simplified setting of individual properties by passing the whole object and setting it to null on complete

--- app/scripts/controllers/addActivityCtrl.js
+++ app/scripts/controllers/addActivityCtrl.js
@@ -15,48 +15,29 @@ function AddActivityCtrl($scope, $http) {
             var tx = db.transaction("activities", "readwrite");
             var store = tx.objectStore("activities");
 
-            store.put({
-                contactDate: $scope.activity.contactDate,
-                activityType: $scope.activity.activityType,
-                contactType: $scope.activity.contactType,
-                contactMethod: $scope.activity.contactMethod,
-                other: $scope.activity.other,
-                employer: $scope.activity.employer,
-                position: $scope.activity.position,
-                phone: $scope.activity.phone,
-                address: $scope.activity.address,
-                city: $scope.activity.city,
-                state: $scope.activity.state,
-                contact: $scope.activity.contact,
-                website: $scope.activity.website,
-                email: $scope.activity.email,
-                jobRefNumber: $scope.activity.jobRefNumber,
-                fax: $scope.activity.fax,
-                newspaperName: $scope.activity.newspaperName,
-                publicationDate: $scope.activity.publicationDate,
-                activityDescription: $scope.activity.activityDescription
-            });                
+            store.put($scope.activity);
             
             tx.oncomplete = function() {
-                $scope.activity.contactDate = '';
-                $scope.activity.activityType = '';
-                $scope.activity.contactType = '';
-                $scope.activity.contactMethod = '';
-                $scope.activity.other = '';
-                $scope.activity.employer = '';
-                $scope.activity.position = '';
-                $scope.activity.phone = '';
-                $scope.activity.address = '';
-                $scope.activity.city = '';
-                $scope.activity.state = '';
-                $scope.activity.contact = '';
-                $scope.activity.website = '';
-                $scope.activity.email = '';
-                $scope.activity.jobRefNumber = '';
-                $scope.activity.fax = '';
-                $scope.activity.newspaperName = '';
-                $scope.activity.publicationDate = '';
-                $scope.activity.activityDescription = '';
+                $scope.activity = null;
+//                $scope.activity.contactDate = '';
+//                $scope.activity.activityType = '';
+//                $scope.activity.contactType = '';
+//                $scope.activity.contactMethod = '';
+//                $scope.activity.other = '';
+//                $scope.activity.employer = '';
+//                $scope.activity.position = '';
+//                $scope.activity.phone = '';
+//                $scope.activity.address = '';
+//                $scope.activity.city = '';
+//                $scope.activity.state = '';
+//                $scope.activity.contact = '';
+//                $scope.activity.website = '';
+//                $scope.activity.email = '';
+//                $scope.activity.jobRefNumber = '';
+//                $scope.activity.fax = '';
+//                $scope.activity.newspaperName = '';
+//                $scope.activity.publicationDate = '';
+//                $scope.activity.activityDescription = '';
                 $scope.addActivityForm.$setPristine();
                 $scope.$apply();
             };
